cfcd76bc7e298eef0729e6c06bd3796fdaffe625
yuko-n <alsuha1505@gmail.com>
2022-02-07 22:49:55 +0700

    matrix

---
+++ test.cpp
@@ -0,0 +1,25 @@
+int main(){
+	ifstream file("C:/Users/yarob/Downloads/input.txt");
+	int size;
+	file >> size;
+	double**  matrix;
+	double* col;
+	double* res;
+	res = new double[size];
+	col = new double[size];
+	matrix = new double*[size];
+	for (int i =0;i<size;i++){
+		matrix[i] = new double[size];
+	}
+	cout << "Matrix:\n";
+	for (int i=0; i<size; i++){
+		for(int j=0; j<size+1 ;j++){
+			file >> matrix[i][j];
+			cout << matrix[i][j] << '\t';
+		}
+		cout << endl;
+	}
+	cout << "\n";
+	gauss(matrix, col, size);
+	return 0;
+}
